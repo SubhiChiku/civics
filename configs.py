63cfe3e752b9efe2c152220b1f55a1c0de3635a3
Ayanokozii <ayanokozi0925@gmail.com>
2024-07-12 23:46:38 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import getenv
 class Config:
     API_ID = int(getenv("API_ID", "21189715"))
     API_HASH = getenv("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7264877354:AAEvVWLnll1U3yusdnz_jLtatOaPIexWLJ0")
+    BOT_TOKEN = getenv("BOT_TOKEN", "7264877354:AAHSvWE2Kzw5mFNsEbdPzPyoVJafj6ZRSAE")
     CHID = int(getenv("CHID", "-1002149291828"))
     SUDO = list(map(int, getenv("SUDO", "6798912985").split(",")))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
