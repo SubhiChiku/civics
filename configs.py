653fdb1c3fa212ba8f735702cdaadfe12032d88c
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-07-13 13:52:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -3,7 +3,7 @@ from os import getenv
 class Config:
     API_ID = int(getenv("API_ID", "21189715"))
     API_HASH = getenv("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")
-    BOT_TOKEN = getenv("BOT_TOKEN", "7485296857:AAHdzDHIK_qRsGTiOIvXlXTnNqbcbXSIG5E)
+    BOT_TOKEN = getenv("BOT_TOKEN", "7485296857:AAEL_5uTPiaPbIt9iNmAhv96OWHV6ObE1J4)
     CHID = int(getenv("CHID", "-1002152429971"))
     SUDO = list(map(int, getenv("SUDO", "6798912985").split(",")))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
