20882e1f460900529c2198c5d02fdd4bf0627e61
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-07-14 21:10:56 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
-from pyrogram import Client, filters, enums, errors
+from pyrogram import Client, filters, enums
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
-from pyrogram.errors.exceptions.flood_420 import FloodWait
+from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked
 from database import add_user, add_group, all_users, all_groups, users, remove_user
 from configs import cfg
 import asyncio
@@ -14,24 +14,32 @@ app = Client(
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Main process ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-@app.on_chat_join_request(filters.group | filters.channel & ~filters.private)
-async def approve(_, m: Message):
+@app.on_chat_join_request(filters.group | filters.channel)
+async def approve(client, m: Message):
     op = m.chat
     user = m.from_user
     try:
         add_group(m.chat.id)
-        await app.approve_chat_join_request(op.id, user.id)
-        await app.send_message(user.id, f"**Hello {user.mention}!\nWelcome to {m.chat.title}**")
+        await client.approve_chat_join_request(op.id, user.id)
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/free_request_accepting_bot?startgroup=true"),
+                    InlineKeyboardButton("➕ Add me to your Channel ➕", url="https://t.me/free_request_accepting_bot?startchannel=true")
+                ]
+            ]
+        )
+        await client.send_message(user.id, 
+                                  f"**Hello {user.mention}!\nWelcome to {m.chat.title}**",
+                                  reply_markup=keyboard)
         add_user(user.id)
-    except errors.PeerIdInvalid:
-        print("User isn't started the bot (means group).")
     except Exception as err:
         print(f"Error: {err}")
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Start ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.command("start"))
-async def start(_, m: Message):
+async def start(client, m: Message):
     try:
         if m.chat.type == enums.ChatType.PRIVATE:
             keyboard = InlineKeyboardMarkup(
@@ -47,12 +55,11 @@ async def start(_, m: Message):
                 f"**🦊 Hello {m.from_user.mention}!\nI'm an auto-approve Admin Join Requests Bot.\nI can approve users in Groups/Channels. Add me to your chat and promote me to admin with add members permission.**",
                 reply_markup=keyboard
             )
-    
         elif m.chat.type in (enums.ChatType.GROUP, enums.ChatType.SUPERGROUP):
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url=f"https://t.me/free_request_accepting_bot?start=true")
+                        InlineKeyboardButton("💁‍♂️ Start me private 💁‍♂️", url="https://t.me/free_request_accepting_bot?start=true")
                     ]
                 ]
             )
@@ -61,22 +68,21 @@ async def start(_, m: Message):
                 f"**🦊 Hello {m.from_user.first_name}!\nWrite me private for more details.**",
                 reply_markup=keyboard
             )
-        
         print(f"{m.from_user.first_name} has started your bot!")
-
     except Exception as err:
         print(f"Error: {err}")
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Callback ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
-@app.on_callback_query(filters.regex("chk"))
-async def chk(_, cb: CallbackQuery):
+@app.on_callback_query(filters.regex("button1|button2"))
+async def chk(client, cb: CallbackQuery):
     try:
         if cb.message.chat.type == enums.ChatType.PRIVATE:
+            bot_username = (await client.get_me()).username
             keyboard = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("➕ Add me to your Chat ➕", url=f"https://t.me/{app.username}?startgroup=true")
+                        InlineKeyboardButton("➕ Add me to your Chat ➕", url=f"https://t.me/{bot_username}?startgroup=true")
                     ]
                 ]
             )
@@ -86,16 +92,14 @@ async def chk(_, cb: CallbackQuery):
                 reply_markup=keyboard,
                 disable_web_page_preview=True
             )
-        
-        print(f"{cb.from_user.first_name} has started your bot!")
-    
+        print(f"{cb.from_user.first_name} has interacted with your bot!")
     except Exception as e:
         print(f"Error: {e}")
 
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Info ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.command("users") & filters.user(cfg.SUDO))
-async def dbtool(_, m: Message):
+async def dbtool(client, m: Message):
     user_count = all_users()
     group_count = all_groups()
     total_count = user_count + group_count
@@ -108,7 +112,7 @@ async def dbtool(_, m: Message):
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Broadcast ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.command("bcast") & filters.user(cfg.SUDO))
-async def bcast(_, m: Message):
+async def bcast(client, m: Message):
     all_users_data = users
     response_message = await m.reply_text("`⚡️ Processing...`")
     success = 0
@@ -124,10 +128,10 @@ async def bcast(_, m: Message):
         except FloodWait as ex:
             await asyncio.sleep(ex.x)
             await m.reply_to_message.copy(int(user_id))
-        except errors.InputUserDeactivated:
+        except InputUserDeactivated:
             deactivated += 1
             remove_user(user_id)
-        except errors.UserIsBlocked:
+        except UserIsBlocked:
             blocked += 1
         except Exception as e:
             print(f"Error: {e}")
@@ -140,7 +144,7 @@ async def bcast(_, m: Message):
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Broadcast Forward ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.command("fcast") & filters.user(cfg.SUDO))
-async def fcast(_, m: Message):
+async def fcast(client, m: Message):
     all_users_data = users
     response_message = await m.reply_text("`⚡️ Processing...`")
     success = 0
@@ -156,10 +160,10 @@ async def fcast(_, m: Message):
         except FloodWait as ex:
             await asyncio.sleep(ex.x)
             await m.reply_to_message.forward(int(user_id))
-        except errors.InputUserDeactivated:
+        except InputUserDeactivated:
             deactivated += 1
             remove_user(user_id)
-        except errors.UserIsBlocked:
+        except UserIsBlocked:
             blocked += 1
         except Exception as e:
             print(f"Error: {e}")
@@ -172,15 +176,15 @@ async def fcast(_, m: Message):
 #━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ Accept Pending Requests on Startup ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 
 @app.on_message(filters.command("start") & filters.user(cfg.SUDO))
-async def accept_pending_requests(_, m: Message):
-    groups = await app.get_dialogs()
-    for dialog in groups:
+async def accept_pending_requests(client, m: Message):
+    dialogs = await client.get_dialogs()
+    for dialog in dialogs:
         if dialog.chat.type in [enums.ChatType.SUPERGROUP, enums.ChatType.CHANNEL]:
             try:
-                pending_requests = await app.get_chat_join_requests(dialog.chat.id)
+                pending_requests = await client.get_chat_join_requests(dialog.chat.id)
                 for request in pending_requests:
-                    await app.approve_chat_join_request(dialog.chat.id, request.user.id)
-                    await app.send_message(request.user.id, f"**Hello {request.user.mention}!\nWelcome to {dialog.chat.title}**")
+                    await client.approve_chat_join_request(dialog.chat.id, request.user.id)
+                    await client.send_message(request.user.id, f"**Hello {request.user.mention}!\nWelcome to {dialog.chat.title}**")
                     add_user(request.user.id)
                     add_group(dialog.chat.id)
             except Exception as e:
