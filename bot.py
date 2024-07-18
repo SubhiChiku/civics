f1b163441772bba4b4463bab8141a837fbd692d8
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2024-07-18 11:37:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,9 @@ async def approve(client, m: Message):
         keyboard = InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/free_request_accepting_bot?startgroup=true"),
+                    InlineKeyboardButton("➕ Add me to your Chat ➕", url="https://t.me/free_request_accepting_bot?startgroup=true"), 
+                ],
+                [
                     InlineKeyboardButton("➕ Add me to your Channel ➕", url="https://t.me/free_request_accepting_bot?startchannel=true")
                 ]
             ]
