760620c2c2cd8adac9f9bf1ca18c85311bc32cd1
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2025-01-03 15:35:24 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,47 +1,24 @@
 from pymongo import MongoClient
-from configs import cfg
 
-client = MongoClient(cfg.MONGO_URI)
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/mydatabase")  # Replace with your MongoDB URI
+db = client["free_request_accepter_bot"]  # Replace with your database name
+user_collection = db["users"]  # Collection name for users
 
-users = client['main']['users']
-groups = client['main']['groups']
-
-def already_db(user_id):
-        user = users.find_one({"user_id" : str(user_id)})
-        if not user:
-            return False
-        return True
-
-def already_dbg(chat_id):
-        group = groups.find_one({"chat_id" : str(chat_id)})
-        if not group:
-            return False
-        return True
+def users():
+    """Returns a list of user IDs stored in the database."""
+    return [user["user_id"] for user in user_collection.find({}, {"_id": 0, "user_id": 1})]
 
 def add_user(user_id):
-    in_db = already_db(user_id)
-    if in_db:
-        return
-    return users.insert_one({"user_id": str(user_id)}) 
-
-def remove_user(user_id):
-    in_db = already_db(user_id)
-    if not in_db:
-        return 
-    return users.delete_one({"user_id": str(user_id)})
-    
-def add_group(chat_id):
-    in_db = already_dbg(chat_id)
-    if in_db:
-        return
-    return groups.insert_one({"chat_id": str(chat_id)})
+    """Adds a new user to the database."""
+    if not user_collection.find_one({"user_id": user_id}):
+        user_collection.insert_one({"user_id": user_id})
 
 def all_users():
-    user = users.find({})
-    usrs = len(list(user))
-    return usrs
+    """Returns the total count of users."""
+    return user_collection.count_documents({})
 
-def all_groups():
-    group = groups.find({})
-    grps = len(list(group))
-    return grps
+def remove_user(user_id):
+    """Removes a user from the database."""
+    user_collection.delete_one({"user_id": user_id})
+        
