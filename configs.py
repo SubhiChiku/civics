62751807c7614c1740cf99e772f58f6a3d26f5de
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2025-01-03 13:53:41 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,5 +6,5 @@ class Config:
     BOT_TOKEN = getenv("BOT_TOKEN", "7485296857:AAEpfLsEF48O_-8iepYVI_wp2zhVMXaQLR4")
     SUDO = list(map(int, getenv("SUDO", "6798912985,1069898029").split(",")))
     MONGO_URI = getenv("MONGO_URI", "mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/mydatabase")
-    LOGGER_GROUP_ID = int(getrnv("LOGGER_GROUP_ID" , "-1002231147120"))
+    LOGGER_GROUP_ID = int(getenv("LOGGER_GROUP_ID" , "-1002231147120"))
 cfg = Config()
