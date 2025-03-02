d103b20e870d389d2009f52402965ac4dd0ffcb7
King the perse <141738897+SubhiChiku@users.noreply.github.com>
2025-03-02 21:32:48 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    API_ID = int(os.getenv("API_ID", "0"))  # Default to 0 if not set
-    API_HASH = os.getenv("API_HASH", "")  # Empty string as default
-    BOT_TOKEN = os.getenv("BOT_TOKEN", "")  # Empty string as default
-    SUDO = list(map(int, filter(None, os.getenv("SUDO", "").replace(" ", "").split(","))))  # Removes spaces and handles empty case
-    MONGO_URI = os.getenv("MONGO_URI", "")
-    LOGGER_GROUP_ID = int(os.getenv("LOGGER_GROUP_ID", "-1000000000000"))  # Default to a valid Telegram group ID
+    API_ID = int(os.getenv("API_ID", "21189715"))  
+    API_HASH = os.getenv("API_HASH", "988a9111105fd2f0c5e21c2c2449edfd")  
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "7485296857:AAEpfLsEF48O_-8iepYVI_wp2zhVMXaQLR4")  
+    SUDO = list(map(int, filter(None, os.getenv("SUDO", "6798912985,1069898029,7225660023").replace(" ", "").split(","))))  
+    MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://ayanosuvii0925:subhichiku123@cluster0.uw8yxkl.mongodb.net/mydatabase")  
+    LOGGER_GROUP_ID = int(os.getenv("LOGGER_GROUP_ID", "-1002341427688"))  
 
 cfg = Config()
